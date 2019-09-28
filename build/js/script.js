9d3cbaa70b4462ef637ba21eef1ba1beeee55b0b
Bogdan <dastan_krimea@mail.ru>
2019-09-28 09:08:57 +0300

    reserve

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,35 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
-
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+(function () {
+  var formScreens = document.querySelectorAll('.form > div');
+  var buttonsNext = document.querySelectorAll('.button-next');
+  var buttonsPrev = document.querySelectorAll('.button-prev');
+  if (window.NodeList && !NodeList.prototype.forEach) {
+    NodeList.prototype.forEach = Array.prototype.forEach;
   }
-});
+
+  buttonsNext.forEach(function (element, index) {
+    element.addEventListener('click', function (evt) {
+      var activeScreen = document.querySelector('.form-activeScreen');
+      evt.preventDefault();
+
+      activeScreen.classList.add('visually-hidden');
+      activeScreen.classList.remove('form-activeScreen');
+      formScreens[index + 1].classList.add('form-activeScreen');
+      formScreens[index + 1].classList.remove('visually-hidden');
+    });
+  });
+
+   buttonsPrev.forEach(function (element, index) {
+    element.addEventListener('click', function (evt) {
+      var activeScreen = document.querySelector('.form-activeScreen');
+      evt.preventDefault();
+
+      activeScreen.classList.add('visually-hidden');
+      activeScreen.classList.remove('form-activeScreen');
+      formScreens[index].classList.add('form-activeScreen');
+      formScreens[index].classList.remove('visually-hidden');
+    });
+  });
+
+})();
